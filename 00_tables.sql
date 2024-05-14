e9f8a31028158533355bd2e463403e952ecfc9c7
bnowacki <bknowacki@gmail.com>
2024-05-14 14:53:55 +0200

    preview queries

--- 00_tables.sql
+++ 00_tables.sql
@@ -31,8 +31,8 @@ CREATE TABLE companies (
 );
 
 CREATE TABLE company_users (
-    user_id INT REFERENCES users(id),
-    company_id INT REFERENCES companies(id),
+    user_id INT REFERENCES users(id) NOT NULL,
+    company_id INT REFERENCES companies(id) NOT NULL,
     role VARCHAR2(5) NOT NULL CHECK (role IN ('owner', 'admin', 'basic')),
 
     PRIMARY KEY (user_id, company_id)
@@ -51,15 +51,15 @@ CREATE TABLE service_steps (
     id INT PRIMARY KEY,
     name VARCHAR2(255) NOT NULL,
     description VARCHAR2(2000),
-    service_id INT REFERENCES services(id),
     department_id INT REFERENCES departments(id) NOT NULL,
     input_definition CLOB CHECK (input_definition IS JSON)
 );
 
 CREATE TABLE service_step_dependencies (
-    service_step_id INT REFERENCES service_steps(id),
+    service_step_id INT REFERENCES service_steps(id) NOT NULL,
     depends_on INT REFERENCES service_steps(id),
-    PRIMARY KEY (service_step_id, depends_on)
+    service_id INT REFERENCES services(id) NOT NULL,
+    PRIMARY KEY (service_step_id, depends_on, service_id)
 );
 
 -- orders
@@ -75,10 +75,10 @@ CREATE TABLE orders (
 
 CREATE TABLE tasks (
     id INT PRIMARY KEY,
-    service_step_id INT REFERENCES service_steps(id),
+    service_step_id INT REFERENCES service_steps(id) NOT NULL,
     status VARCHAR2(15) NOT NULL CHECK (status IN ('blocked', 'to_do', 'in_progress', 'hold', 'cancelled', 'failed', 'done')),
     status_changed_at TIMESTAMP,
-    order_id INT REFERENCES orders(id),
+    order_id INT REFERENCES orders(id) NOT NULL,
     assignee INT REFERENCES users(id),
     input_data CLOB CHECK (input_data IS JSON)
 );
@@ -86,7 +86,7 @@ CREATE TABLE tasks (
 CREATE OR REPLACE VIEW task_dependencies AS
 SELECT
     t1.id AS task_id,
-    t1.id AS dependent_task_id
+    t2.id AS depends_on
 FROM
     tasks t1
 JOIN
